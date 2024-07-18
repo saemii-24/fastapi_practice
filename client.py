5daf9dc7053f1916fcb1dba2760bb9ce8fb56af8
saemii-24 <hims2419@gmail.com>
2024-07-18 20:38:13 +0900

    Feat: 쿼리 파라미터

--- client.py
+++ client.py
@@ -1,10 +1,15 @@
 import requests
 
-res = requests.get("http://127.0.0.1:8000/items/sample")
+# res = requests.get("http://127.0.0.1:8000/items/sample")
+
+# print(res.status_code)
+# print(res.text)
+
+#http://127.0.0.1:8000/docs
+#http://127.0.0.1:8000/docs
+
+
+res = requests.get("http://127.0.0.1:8000/items")
 
 print(res.status_code)
 print(res.text)
-
-#http://127.0.0.1:8000/docs
-#http://127.0.0.1:8000/docs
-
--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 #uvicorn main:app --reload
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Annotated
 
 app = FastAPI()
 
 #아래의 경우 client 요청 경로가 items/sample로 동일할 수 있다. 
 #이때는 >>먼저 쓰여진 것<<이 불러와진다.
 
+# /items/sample 경로로 요청이 들어올 때
 @app.get("/items/sample")
 def read_sample():
-  return {"sample_id": "샘플", "sample_name": "샘플",}
+    return {"sample_id": "샘플", "sample_name": "샘플"}
 
+# /items/{item_id} 경로로 요청이 들어올 때
 @app.get("/items/{item_id}")
-def read_item(item_id):
+def read_item(item_id: int):
     return {"item_id": item_id, "item_name": "티셔츠"}
+
+items = ["티셔츠", "스커트", "부츠", "코트"]
+
+# /items 경로로 요청이 들어올 때, skip과 limit 쿼리 매개변수를 사용
+@app.get("/items")
+#ge = 이상 / le = 이하 | gt = 초과 / lt = 미만
+def read_items(skip: int = 0, limit: Annotated[int, Query(ge=1, le=10)] = 10):
+    return {"items": items[skip:skip + limit]}

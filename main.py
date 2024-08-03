918e83ec161d7d9e0a722fff79c9ec0e1c5784ff
saemii-24 <hims2419@gmail.com>
2024-08-03 23:23:31 +0900

    Feat: 쿼리 매개변수

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # fastapi 임포트
+from typing import Union
 from fastapi import FastAPI
 
 # fastapi 인스턴스 생성
@@ -45,3 +46,41 @@ async def read_item(item_id):
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+#쿼리 매개변수
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+#파이썬 리스트를 이용해 특정 범위의 아이템을 선택
+#슬라이싱 구문 start:stop은 리스트의 start 인덱스부터 stop-1 인덱스까지의 아이템을 포함
+
+
+#http://127.0.0.1:8000/items/?skip=0&limit=10
+
+#선택적 매개변수
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Union[str, None] = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+#쿼리 매개변수 형변환
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+#필수 쿼리 매개변수
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
